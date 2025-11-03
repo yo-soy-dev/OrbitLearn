aba393fe82ba6f15b7e29af662fb09e8b23cc2fe
Devansh Tiwari <devanshtiwari817@gmail.com>
2025-11-03 21:12:04 +0530

    Project Completed

--- src/app/companions/new/page.tsx
+++ src/app/companions/new/page.tsx
@@ -10,7 +10,7 @@ const NewCompanion = async () => {
   const hasPermission = await newCompanionPermissions();
 
   return (
-    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+    <main className="min-lg:w-1/3 min-md:w-2/3 mx-auto flex flex-col items-center justify-center pb-16">
       {hasPermission ? (
       <article className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
